69abedeec806b68b80b498a36d9765184e906cfb
Varun Shastry <varun.shastry@stonybrook.edu>
2014-10-16 14:35:55 -0400

    Intermediate commit works for below algos
    1. Bruteforce
    2. Team's solution (out own logic)
    3. MRV heuristic
    
    Signed-off-by: Varun Shastry <varun.shastry@stonybrook.edu>

--- Project 2/CSP/sudokuSolver.py
+++ Project 2/CSP/sudokuSolver.py
@@ -20,7 +20,34 @@ from sudokuUtil import *
 # (basic backtracking or with MRV and forward checking).
 # For example: python sudokuSolver.py backtracking
 
+nodesExpanded = 0
 global_puzzle = []
+order_domain_values = [[[] for x in xrange(9)] for x in xrange(9)]
+
+#########################################################################
+##################### General methods used by all algos #################
+#########################################################################
+
+# Established the solution domain by traversing through the sudoku
+# Creates a list for each empty cell by the order domain values
+# and removes the values which are inconsistent
+def establishSolutionDomain(puzzle):
+    global order_domain_values
+
+    for i in range(0,9):
+        for j in range (0,9):
+            if puzzle[i][j] == 0:
+                for var in range(1,10):
+                    # not (inRow or inColumn or inBox)
+                    inRow = isInRow(puzzle, i, var)
+                    inColumn = isInColumn (puzzle, j, var)
+                    inBox = isInBox (puzzle, i, j, var)
+
+                    if not (inRow or inColumn or inBox):
+                        order_domain_values[i][j].append(var)
+
+    print order_domain_values
+    return
 
 # Returns the total number of unsolved Cells, ie counts for the number of '0's
 # in the puzzle array
@@ -61,13 +88,23 @@ def isInBox(puzzle, row, column, var):
 
     return False
 
+#########################################################################
+##################### different methods to serve domain values ##########
+#########################################################################
+def getOrderDomainValues():
+    return [1,2,3,4,5,6,7,8,9]
+
+# this is common for all MRV, FC and Arc
+def getOrderDomainValuesMRV (orderDomainValues, row, column):
+    return orderDomainValues[row][column]
+
 # A list generated for a cell in the 9x9 sudoku which says what are the
 # numbers can be filled in the cell (takes those number which are not in any
-# of current row, current column or current sub 3x3 box)
+# of current row, current column or current sub 3x3 box) for Team's solution
 def getCanBeFilledList(puzzle, row, column):
     canBeFilledList = []
 
-    for i in range(0,10):
+    for i in range(1,10):
         # not (inRow or inColumn or inBox)
         inRow = isInRow(puzzle, row, i)
         inColumn = isInColumn (puzzle, column, i)
@@ -78,8 +115,32 @@ def getCanBeFilledList(puzzle, row, column):
 
     return canBeFilledList
 
-# Returns the next cell that needs to be solved
-def getNextEmptyCell(puzzle, curRow, curCol):
+
+#########################################################################
+##################### Selects the next cell that needs to be solved #####
+#########################################################################
+
+# This is the main logic of MRV which returns the cell with least
+# possible order domain values by minimizing the no of wrong tree path
+# expansion, we can also use this in the FC and Arc consistency algos
+def selectUnassignedVariableMRV(puzzle, orderDomainValues, row, column):
+    min = (-1, -1)
+    minLen = 10
+
+    for i in range(0,9):
+        for j in range(0,9):
+            if i == row and j == column:
+                continue
+
+            if puzzle[i][j] == 0:
+                if len(orderDomainValues[i][j]) < minLen:
+                    min = (i, j)
+                    minLen = len(orderDomainValues[i][j])
+
+    return min
+
+# Returns the next cell that needs to be solved without any intelligence
+def selectUnassignedVariable(puzzle, curRow, curCol):
 
     for j in range(curCol, 9):
         if puzzle[curRow][j] == 0:
@@ -92,8 +153,115 @@ def getNextEmptyCell(puzzle, curRow, curCol):
 
     return (-1,-1)
 
+
+#########################################################################
+##################### consistency check methods #########################
+#########################################################################
+
+#Consistency check
+# This checks whtether a value can be assigned at the current row and column
+# based on the puzzle inputs
+# How does it do?
+# Checks whether the given value already present in the given row/column/box
+def isConsistent (var, puzzle, row, column):
+    inRow = isInRow(puzzle, row, var)
+    inColumn = isInColumn (puzzle, column, var)
+    inBox = isInBox (puzzle, row, column, var)
+
+    if (inRow or inColumn or inBox):
+        return False
+
+    return True
+
+# Need not to check for consistency in case of MRV/FC/Arc as we prepare the solution domain
+# with consistent values, so below method call is a no-op
+def isConsistentMRV(a,b,c,d):
+    return True
+
+
+###################################################################################################
+##################### Inference Algos for FC and Arc #############################################
+###################################################################################################
+
+# Does an inference for Arc consistency
+# Constraint propagation
+def doInferenceArc(var, orderDomainValues, row, column):
+    orderDomainValues[row][column].remove(var)
+
+    # Contraint remove don only if the constraint left with single value
+    if len(orderDomainValues[row][column]) != 1:
+        return
+
+    # Using DFS graph search clear all the constraints from the solution domain
+    visitedCells = []
+    fringe = []
+    fringe.append ((row, column))
+
+    while (len (fringe) == 0):
+        lrow, lcolumn = fringe.pop ()
+        lvar = orderDomainValues[lrow][lcolumn][0]
+        visitedCells.append ((lrow, lcolumn, lvar))
+
+        # Remove from the corresponding row
+        for j in range(0,9):
+            orderDomainValues[lrow][j].remove (lvar)
+            if len (orderDomainValues[lrow][j]) == 1 and not((lrow, j, lvar) in visitedCells):
+                fringe.append (( row, j))
+
+        # Remove from the corresponding column
+        for i in range(0,9):
+            orderDomainValues[i][lcolumn].remove (lvar)
+            if len (orderDomainValues[i][lcolumn]) == 1 and not((lrow, j, lvar) in visitedCells):
+                fringe.append ((i, lcolumn))
+
+        boxStartX = ((lrow/3) * 3)
+        boxStartY = ((lcolumn/3) * 3)
+
+        # Remove from the corresponding box
+        for i in range(boxStartX,(boxStartX + 3)):
+            for j in range(boxStartY,(boxStartY + 3)):
+                orderDomainValues[i][j].remove (lvar)
+                if len (orderDomainValues[i][j]) == 1 and not((lrow, j, lvar) in visitedCells):
+                    fringe.append ((i, j))
+
+# Does an inference for Forward Checking
+def doInferenceFC(var, orderDomainValues, row, column):
+    orderDomainValues[row][column].remove(var)
+
+    # Contraint remove don only if the constraint left with single value
+    if len(orderDomainValues[row][column]) != 1:
+        return
+
+    # Remove from the corresponding row
+    for j in range(0,9):
+        if var in orderDomainValues[row][j]:
+            orderDomainValues[row][j].remove (var)
+
+    # Remove from the corresponding column
+    for i in range(0,9):
+        if var in orderDomainValues[i][column]:
+            orderDomainValues[i][column].remove (var)
+
+    boxStartX = ((row/3) * 3)
+    boxStartY = ((column/3) * 3)
+
+    # Remove from the corresponding box
+    for i in range(boxStartX,(boxStartX + 3)):
+        for j in range(boxStartY,(boxStartY + 3)):
+            if var in orderDomainValues[i][j]:
+                orderDomainValues[i][j].remove (var)
+
+    print orderDomainValues
+    return
+
+###################################################################################################
+##################### Main algos that checks responisble for backtracking #########################
+###################################################################################################
 # Recursive backtrack algorithm
-def basicBacktrackSudokuSolver(noOfUnsolvedCells, row, column):
+def oursolutionSudokuSolver(noOfUnsolvedCells, row, column):
+    # To display the statistics
+    global nodesExpanded
+    nodesExpanded += 1
 
     # Terminating condition
     if noOfUnsolvedCells == 0:
@@ -103,14 +271,14 @@ def basicBacktrackSudokuSolver(noOfUnsolvedCells, row, column):
         print "Received Unexpected Logic Error or Unsolvable input puzzle"
         sys.exit()
 
-    (nextEmptyX, nextEmptyY) = getNextEmptyCell(global_puzzle, row, column+1)
+    (nextEmptyX, nextEmptyY) = selectUnassignedVariable(global_puzzle, row, column+1)
 
     for i in getCanBeFilledList(global_puzzle, row, column):
 
         global_puzzle[row][column] = i
         noOfUnsolvedCells -= 1
 
-        ret = basicBacktrackSudokuSolver(noOfUnsolvedCells, nextEmptyX, nextEmptyY)
+        ret = oursolutionSudokuSolver(noOfUnsolvedCells, nextEmptyX, nextEmptyY)
         if ret:
             # If successful say it to caller
             return True
@@ -120,17 +288,278 @@ def basicBacktrackSudokuSolver(noOfUnsolvedCells, row, column):
 
     return False
 
+# Arc consistency added to the naive backtrack method
+def arcconsistencyBacktrackMethod(noOfUnsolvedCells, row, column):
+
+    # To display the statistics
+    global nodesExpanded
+    nodesExpanded += 1
+
+    # Terminating condition - ie Is the assignment complete?
+    # If so terminate the control by returning 'assignment'.
+    if noOfUnsolvedCells == 0:
+        return True
+
+    # Check for error condition
+    if row == -1 or column == -1:
+        print "Received Unexpected Logic Error or Unsolvable input puzzle"
+        sys.exit()
+
+    # Select-Unassigned-Variable - returns the next empty cell with minimum remaining values (MRV)
+    (nextEmptyX, nextEmptyY) = selectUnassignedVariableMRV (global_puzzle, order_domain_values, row, column)
+
+    orderDomainValues = getOrderDomainValuesMRV (order_domain_values, row, column);
+
+    for i in orderDomainValues:
+        # Check for consistency - Look at the function definition for consistency detail
+        if isConsistentMRV (i, global_puzzle, row, column):
+            # If consistent assign the variable
+            global_puzzle[row][column] = i
+            noOfUnsolvedCells -= 1
+
+            # Plese check the what does this function do above the function definition
+            doInferenceArc (i, order_domain_values, row, column)
+
+            ret = arcconsistencyBacktrackMethod(noOfUnsolvedCells, nextEmptyX, nextEmptyY)
+            if ret:
+                # If successful say it to caller
+                return True
+
+            global_puzzle[row][column] = 0
+            noOfUnsolvedCells += 1
+
+    return False
+
+# Forward checking added to the naive backtrack method
+def forwardcheckBacktrackMethod(noOfUnsolvedCells, row, column):
+
+    # To display the statistics
+    global nodesExpanded
+    nodesExpanded += 1
+
+    # Terminating condition - ie Is the assignment complete?
+    # If so terminate the control by returning 'assignment'.
+    if noOfUnsolvedCells == 0:
+        return True
+
+    # Check for error condition
+    if row == -1 or column == -1:
+        print "Received Unexpected Logic Error or Unsolvable input puzzle"
+        sys.exit()
+
+    # Select-Unassigned-Variable - returns the next empty cell with minimum remaining values (MRV)
+    (nextEmptyX, nextEmptyY) = selectUnassignedVariableMRV (global_puzzle, order_domain_values, row, column)
+    print "NextEmpty: ", nextEmptyX, nextEmptyY
+
+    orderDomainValues = getOrderDomainValuesMRV (order_domain_values, row, column);
+
+    for i in orderDomainValues:
+        # Check for consistency - Look at the function definition for consistency detail
+        if isConsistent (i, global_puzzle, row, column):
+            # If consistent assign the variable
+            global_puzzle[row][column] = i
+            noOfUnsolvedCells -= 1
+
+            # Plese check the what does this function do above the function definition
+            doInferenceFC (i, order_domain_values, row, column)
+
+            ret = forwardcheckBacktrackMethod(noOfUnsolvedCells, nextEmptyX, nextEmptyY)
+            if ret:
+                # If successful say it to caller
+                return True
+
+            global_puzzle[row][column] = 0
+            noOfUnsolvedCells += 1
+
+    return False
+
+
+# Question 5 part A
+def mrvBacktrackMethod(noOfUnsolvedCells, row, column):
+
+    # To display the statistics
+    global nodesExpanded
+    nodesExpanded += 1
+
+    # Terminating condition - ie Is the assignment complete?
+    # If so terminate the control by returning 'assignment'.
+    if noOfUnsolvedCells == 0:
+        return True
+
+    # Check for error condition
+    if row == -1 or column == -1:
+        print "Received Unexpected Logic Error or Unsolvable input puzzle"
+        sys.exit()
+
+    # Select-Unassigned-Variable - returns the next empty cell
+    (nextEmptyX, nextEmptyY) = selectUnassignedVariableMRV (global_puzzle, order_domain_values, row, column)
+
+    # For the brute-force method the Order domain is always 1-9
+    orderDomainValues = getOrderDomainValuesMRV (order_domain_values, row, column);
+
+    for i in orderDomainValues:
+        # Check for consistency - Look at the function definition for consistency detail
+        if isConsistent (i, global_puzzle, row, column):
+            # If consistent assign the variable
+            global_puzzle[row][column] = i
+            noOfUnsolvedCells -= 1
+
+            ret = mrvBacktrackMethod(noOfUnsolvedCells, nextEmptyX, nextEmptyY)
+            if ret:
+                # If successful say it to caller
+                return True
+
+            # If failure UN-assign the variable and decrement the noOfUnsolvedCells var
+            global_puzzle[row][column] = 0
+            noOfUnsolvedCells += 1
+
+    # Couldn't find the solution so backtrack and say False to caller
+    return False
+
+# Naive Brute-force backtrack
+# Question 4
+def bruteForceBacktrackMethod(noOfUnsolvedCells, row, column):
+
+    # To display the statistics
+    global nodesExpanded
+    nodesExpanded += 1
+
+    # Terminating condition - ie Is the assignment complete?
+    # If so terminate the control by returning 'assignment'.
+    if noOfUnsolvedCells == 0:
+        return True
+
+    # Check for error condition
+    if row == -1 or column == -1:
+        print "Received Unexpected Logic Error or Unsolvable input puzzle"
+        sys.exit()
+
+    # Select-Unassigned-Variable - returns the next empty cell
+    (nextEmptyX, nextEmptyY) = selectUnassignedVariable (global_puzzle, row, column+1)
+
+    # For the brute-force method the Order domain is always 1-9
+    orderDomainValues = getOrderDomainValues ();
+
+    for i in orderDomainValues:
+        # Check for consistency - Look at the function definition for consistency detail
+        if isConsistent (i, global_puzzle, row, column):
+            # If consistent assign the variable
+            global_puzzle[row][column] = i
+            noOfUnsolvedCells -= 1
+
+            ret = bruteForceBacktrackMethod(noOfUnsolvedCells, nextEmptyX, nextEmptyY)
+            if ret:
+                # If successful say it to caller
+                return True
+
+            # If failure UN-assign the variable and decrement the noOfUnsolvedCells var
+            global_puzzle[row][column] = 0
+            noOfUnsolvedCells += 1
+
+    # Couldn't find the solution so backtrack and say False to caller
+    return False
+
+
+
+# Arc Consistency triggering method
+def arcconsistency(puzzle, noOfUnsolvedCells):
+    establishSolutionDomain (puzzle)
+
+    # Get the minimum remaining value
+    (startX, startY) = selectUnassignedVariableMRV(puzzle, order_domain_values, 0, 0)
+
+    arcconsistencyBacktrackMethod (noOfUnsolvedCells, startX, startY)
+
+    return
+
+# Trigger the forward check algorithm
+def forwardcheck(puzzle, noOfUnsolvedCells):
+    establishSolutionDomain (puzzle)
+
+    (startX, startY) = selectUnassignedVariableMRV(puzzle, order_domain_values, 0, 0)
+
+    forwardcheckBacktrackMethod (noOfUnsolvedCells, startX, startY)
+
+    return
+
+# Triggering the MRV consistency function algorithm
+def mrv(puzzle, noOfUnsolvedCells):
+    establishSolutionDomain (puzzle)
+
+    (startX, startY) = selectUnassignedVariableMRV(puzzle, order_domain_values, 0, 0)
+
+    mrvBacktrackMethod (noOfUnsolvedCells, startX, startY)
+
+    return
+
+# Triggering function for bruteforce algorith
+def oursolution(puzzle, noOfUnsolvedCells):
+
+    (startX, startY) = selectUnassignedVariable(puzzle, 0, 0)
+
+    oursolutionSudokuSolver (noOfUnsolvedCells, startX, startY);
+
+    return
+
+# Triggering function for bruteforce algorith
+def bruteforce(puzzle, noOfUnsolvedCells):
+
+    (startX, startY) = selectUnassignedVariable(puzzle, 0, 0)
+
+    oursolutionSudokuSolver(noOfUnsolvedCells, startX, startY);
+
+    return
 
 def solve_puzzle(puzzle, argv):
     """Solve the sudoku puzzle."""
     global global_puzzle
     global_puzzle = puzzle[:]
 
-    noOfUnsolvedCells = getNoOfUnsolvedCells(global_puzzle)
+    noOfUnsolvedCells = getNoOfUnsolvedCells(puzzle)
+    if len (argv) == 1:
+        print "Wrong argument please specify: either of the below"
+        print "1. bruteforce"
+        print "2. mrv"
+        print "3. forwardcheck"
+        print "4. arconsistency"
+        print "Defaulting to Team's method"
+        print "Solving the puzzle using own method"
+        ret = oursolution(puzzle, noOfUnsolvedCells)
+        print "Nodes Expanded: ", nodesExpanded
 
-    (startX, startY) = getNextEmptyCell(puzzle, 0, 0)
+        return global_puzzle
 
-    ret = basicBacktrackSudokuSolver(noOfUnsolvedCells, startX, startY)
+
+    if argv[1] == "bruteforce":
+        print "Solving the puzzle using Bruteforce Backtrack method"
+        bruteforce(puzzle, noOfUnsolvedCells);
+        print "Nodes Expanded: ", nodesExpanded
+
+    elif argv[1] == "mrv":
+        print "Solving the puzzle using MRV heuristic method"
+        mrv (puzzle, noOfUnsolvedCells);
+        print "Nodes Expanded: ", nodesExpanded
+
+    elif argv[1] == "forwardcheck":
+        print "Solving the puzzle using Forward Checking method"
+        forwardcheck (puzzle, noOfUnsolvedCells);
+        print "Nodes Expanded: ", nodesExpanded
+
+    elif argv[1] == "arcconsistency":
+        print "Solving the puzzle using Arc consistency method"
+        arconsistency (puzzle, noOfUnsolvedCells);
+        print "Nodes Expanded: ", nodesExpanded
+
+    else:
+        print "Wrong argument please specify: either of the below"
+        print "1. bruteforce"
+        print "2. mrv"
+        print "3. forwardcheck"
+        print "4. arconsistency"
+        print "Defaulting to Team's method"
+        print "Solving the puzzle using own method"
+        ret = oursolution(puzzle, noOfUnsolvedCells)
+        print "Nodes Expanded: ", nodesExpanded
 
     return global_puzzle
     #return load_sudoku('given_solution.txt')
