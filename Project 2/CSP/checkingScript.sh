5bd2d53cca1c04301b5844fe500a550bca9af7ba
Varun Shastry <varun.shastry@stonybrook.edu>
2014-10-16 17:18:30 -0400

    Add a script to check the sudoku for 10 iterations
    Signed-off-by: Varun Shastry <varun.shastry@stonybrook.edu>

---
+++ Project 2/CSP/checkingScript.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+mv puzzle.txt puzzle.txt.backup
+mv given_solution.txt given_solution.txt.backup
+mv solution.txt solution.txt.backup
+
+for i in `seq 1 10`; do
+        echo "Team's solution round: $i"
+        python sudokuGenerator.py > /dev/null 2>&1
+
+        python sudokuSolver.py
+        python sudokuChecker.py > /dev/null 2>&1
+
+        if [ $? -ne 0 ]; then
+                echo ""
+                echo ""
+                echo " Error"
+                echo ""
+                echo ""
+        fi
+
+        echo "Bruteforce round: $i"
+        python sudokuSolver.py bruteforce
+        python sudokuChecker.py > /dev/null 2>&1
+
+        if [ $? -ne 0 ]; then
+                echo ""
+                echo ""
+                echo " Error"
+                echo ""
+                echo ""
+        fi
+
+        echo "MRV round: $i"
+        python sudokuSolver.py mrv
+        python sudokuChecker.py > /dev/null 2>&1
+
+        if [ $? -ne 0 ]; then
+                echo ""
+                echo ""
+                echo " Error"
+                echo ""
+                echo ""
+        fi
+
+        echo "Forward check round: $i"
+        python sudokuSolver.py forwardcheck
+        python sudokuChecker.py > /dev/null 2>&1
+
+        if [ $? -ne 0 ]; then
+                echo ""
+                echo ""
+                echo " Error"
+                echo ""
+                echo ""
+        fi
+
+        echo "Arc Consistency round: $i"
+        python sudokuSolver.py arcconsistency
+        python sudokuChecker.py > /dev/null 2>&1
+
+        if [ $? -ne 0 ]; then
+                echo ""
+                echo ""
+                echo " Error"
+                echo ""
+                echo ""
+        fi
+done
+
+mv puzzle.txt.backup puzzle.txt
+mv given_solution.txt.backup given_solution.txt
+mv solution.txt.backup solution.txt
