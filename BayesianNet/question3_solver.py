fb4f5c6f40c8fb1c9d130a811fca3227e6f8f6af
Aashray Arora <aashray.arora@stonybrook.edu>
2014-11-20 19:15:52 -0500

    Project 3

--- BayesianNet/question3_solver.py
+++ BayesianNet/question3_solver.py
@@ -1,6 +1,7 @@
 class Question3_Solver:
     def __init__(self, cpt):
         self.first_table = dict()
+        self.second_table = dict()
         self.cpt = cpt
         self.letters = ['`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
         self.build_first_table()
@@ -13,22 +14,22 @@ class Question3_Solver:
                 for hid in self.letters:
                     summation = summation + (self.cpt.conditional_prob(X, hid) * self.cpt.conditional_prob(hid, Y));
 
-                self.firsttab[X][Y] = summation
+                self.first_table[(X,Y)] = summation
 
-    def getfirsttab(X, Y):
-        return self.firsttab[self.letters.index[X]][self.letters.index[Y]];
+    #def getfirsttab(X, Y):
+        #return self.firsttab[self.letters.index[X]][self.letters.index[Y]];
 
-    def build_second_table():
+    def build_second_table(self):
         summation = 0
         for X in self.letters:
             for Y in self.letters:
                 for hid in self.letters:
-                    summation = summation + (self.cpt.conditional_prob(X, hid) * self.getfirsttab(hid, Y));
+                    summation = summation + (self.cpt.conditional_prob(X, hid) * self.first_table[(hid,Y)]);
+                    #print(X,Y,hid)
+                self.second_table[(X,Y)] = summation
 
-                self.secondtab[X][Y] = summation
-
-    def getsecondtab(X, Y):
-        return self.secondtab[self.letters.index[X]][self.letters.index[Y]];
+    #def getsecondtab(X, Y):
+        #return self.secondtab[self.letters.index[X]][self.letters.index[Y]];
 
     #####################################
     # ADD YOUR CODE HERE
