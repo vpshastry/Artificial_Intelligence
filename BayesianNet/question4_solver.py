a119451bed6e2f3c14d78935a29aa33888ff176c
Varun Shastry <varun@fsl.cs.sunysb.edu>
2014-11-20 23:25:10 -0500

    add comments
    Signed-off-by: Varun Shastry <varun@fsl.cs.sunysb.edu>

--- BayesianNet/question4_solver.py
+++ BayesianNet/question4_solver.py
@@ -1,41 +1,65 @@
+#from pprint import pprint
 class Question4_Solver:
     def __init__(self, cpt):
+        # To store first order function
         self.first_table = dict()
+
+        # To store second order function
         self.second_table = dict()
+
         self.cpt = cpt
+
+        # Letters to build the first and second order functions
         self.letters = ['`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+        # Letters without '`'
         self.letters_main = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+        # Build both the tables
         self.build_first_table()
         self.build_second_table()
+
+        # Helpful for examining the table, avoid otherwise
         #pprint(self.first_table)
         #pprint(self.second_table)
 
+    # Build such that, if the query is "d-_e"
+    # self.opt.conditional_prob('d', '`') *
+    # self.first_table[(MissinLet, 'd')] *
+    # self.opt.conditional_prob('e', MissinLet) *
+    # self.opt.conditional_prob('`', 'e')
     def build_first_table(self):
 
+        # Build 2 dimensional table with len(letters) * len(letters)
         for X in self.letters:
             for Y in self.letters:
                 summation = 0
+                # Hid is the hidden variable in consideration
                 for hid in self.letters_main:
                     summation = summation + (self.cpt.conditional_prob(X, hid) * self.cpt.conditional_prob(hid, Y))
-                self.first_table[(X, Y)] = summation
 
-    #def getfirsttab(X, Y):
-        #return self.firsttab[self.letters.index[X]][self.letters.index[Y]];
+                # Add it to the table
+                self.first_table[(X, Y)] = summation
 
     def build_second_table(self):
 
+        # Build 2 dimensional table with len(letters) * len(letters)
         for X in self.letters:
             for Y in self.letters:
                 summation = 0
+                # Hid is the hidden variable in consideration
                 for hid in self.letters_main:
                     summation = summation + (self.cpt.conditional_prob(X, hid) * self.first_table[(hid, Y)])
-                    #print(X,Y,hid)
+
+                # Add it to the table
                 self.second_table[(X,Y)] = summation
 
+    # Given the query and letter gets the conditional probability of the letter placed @underscore with the previous non-hidden variable
     def getPrevProb(self, query, let):
         underScoreIdx = query.index ('_');
         prevfunc = 0;
 
+        # Determine which table to use.
         if query[underScoreIdx-1] == '-':
             if query[underScoreIdx-2] == '-':
                 prevfunc = 2;
@@ -46,6 +70,7 @@ class Question4_Solver:
         else:
             prev = query[underScoreIdx-1];
 
+        # Return the conditional probability of the letter with the previous non-hidden variable
         if prevfunc == 0:
             return self.cpt.conditional_prob(let, prev);
         elif prevfunc == 1:
@@ -53,16 +78,17 @@ class Question4_Solver:
         elif prevfunc == 2:
             return self.second_table[(let, prev)]
         else:
-            #print("ERROR............... Exiting")
             exit()
 
         return 1;
 
 
+    # Given the query and letter gets the conditional probability of the letter placed @underscore with the next non-hidden variable
     def getPostProb(self, query, let):
         underScoreIdx = query.index ('_');
         postfunc = 0;
 
+        # Determine which table to use.
         if query[underScoreIdx+1] == '-':
             if query[underScoreIdx+2] == '-':
                 postfunc = 2;
@@ -73,6 +99,7 @@ class Question4_Solver:
         else:
             post = query[underScoreIdx+1];
 
+        # Return the conditional probability of the letter with the next non-hidden variable
         if postfunc == 0:
             return self.cpt.conditional_prob(post, let);
         elif postfunc == 1:
@@ -80,11 +107,12 @@ class Question4_Solver:
         elif postfunc == 2:
             return self.second_table[(post, let)]
         else:
-            #print("ERROR............... Exiting")
             exit()
 
         return 1;
 
+    # Given the query and letter gets the conditional probability of the letter placed @underscore with the next non-hidden variable
+    #    * multiplied by the conditional probability of the letter placed @underscore with the previous non-hidden variable
     def getProb(self, query, let):
         return self.getPrevProb(query, let) * self.getPostProb(query, let);
 
@@ -116,4 +144,3 @@ class Question4_Solver:
                 final_letter = let;
 
         return final_letter
-        #return "t";
