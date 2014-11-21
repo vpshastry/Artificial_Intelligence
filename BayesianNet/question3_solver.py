83f63b7b8f0c2ad8d297b0a6fead5cbe661d678a
Varun Shastry <varun@fsl.cs.sunysb.edu>
2014-11-20 19:06:25 -0500

    Remove duplicate variable
    Signed-off-by: Varun Shastry <varun@fsl.cs.sunysb.edu>

--- BayesianNet/question3_solver.py
+++ BayesianNet/question3_solver.py
@@ -2,7 +2,7 @@ class Question3_Solver:
     def __init__(self, cpt):
         self.first_table = dict()
         self.cpt = cpt
-        self.letters = ['`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','`']
+        self.letters = ['`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
         self.build_first_table()
 
     def build_first_table(self):
