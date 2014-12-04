c86ae90eba829a7aa07c5b9219d1a791f5990849
Varun Shastry <varun.shastry@stonybrook.edu>
2014-12-04 17:41:03 -0500

    Question 2 Final
    Signed-off-by: Varun Shastry <varun.shastry@stonybrook.edu>

--- Project 4/question2_solver.py
+++ Project 4/question2_solver.py
@@ -21,59 +21,19 @@ class Question2_Solver:
                     self.demrepubHash[(target, curAttribute, char)] = 0
 
         i = 0
-
         for column in zip(*data):
-            print "Working on attribute: ", self.attributes[i]
             j = 0
-            noOfdemQ = noOfdemN = noOfdemY = 0
-            noOfrepQ = noOfrepN = noOfrepY = 0
             for char in column:
-                if data[j][-1] == self.targets[0]:
-                    if char is 'y':
-                        noOfdemY = noOfdemY +1
-                    elif char is 'n':
-                        noOfdemN = noOfdemN +1
-                    elif char is '?':
-                        noOfdemQ = noOfdemQ +1
+                if char == self.targets[0] or char == self.targets[1]:
+                    continue
 
-                elif data[j][-1] == self.targets[1]:
-                    if char is 'y':
-                        noOfrepY = noOfrepY +1
-                    elif char is 'n':
-                        noOfrepN = noOfrepN +1
-                    elif char is '?':
-                        noOfrepQ = noOfrepQ +1
+                self.demrepubHash[(data[j][-1], self.attributes[i], char)] += 1
+                j += 1
 
-                j = j +1
-
-            self.demrepubHash[(self.targets[0], self.attributes[i], 'y')] = noOfdemY
-            self.demrepubHash[(self.targets[0], self.attributes[i], 'n')] = noOfdemN
-            self.demrepubHash[(self.targets[0], self.attributes[i], '?')] = noOfdemQ
-
-            self.demrepubHash[(self.targets[1], self.attributes[i], 'y')] = noOfrepY
-            self.demrepubHash[(self.targets[1], self.attributes[i], 'n')] = noOfrepN
-            self.demrepubHash[(self.targets[1], self.attributes[i], '?')] = noOfrepQ
-            i = i +1
+            i += 1
 
         return
 
-        '''
-            for curValue in column:
-                for char in self.values:
-                    if curValue is char:
-                        for target in self.targets:
-                            #print target, data[i][-1]
-                            if target == data[i][-1]:
-                                #print "am i here?"
-                                # TODO: do smoothing
-                                self.demrepubHash[(target, self.attributes[i], char)] += 1
-                        #if self.demrepubHash[(target, self.attributes[i], char)] is 0:
-            #print target, self.attributes[i], char, "------>", self.demrepubHash[(target, self.attributes[i], char)]
-
-            i = i +1
-        '''
-
-        pprint(self.demrepubHash)
     # Add your code here.
     # Read training data and build your naive bayes classifier
     # Store the classifier in this class
@@ -106,22 +66,14 @@ class Question2_Solver:
 
         for i, attribute in zip(range(len(inList)), self.attributes):
             for target in self.targets:
-
                 denominator = 0
                 for char in self.values:
                     denominator = denominator +self.demrepubHash[(target, attribute, char)]
-                #print denominator
-                #print target, attribute, inList[i], "---->", self.demrepubHash[(target, attribute, inList[i])]
-                if denominator is 0:
-                    #print target, attribute, inList[i]
-                    denominator = 1
 
                 probDemRepub[target] = probDemRepub[target]\
                                           *(float(self.demrepubHash[(target, attribute, inList[i])]) /float(denominator))
 
-            #print "DemRepub: ", probDemRepub[target]
 
-        #print probDemRepub
         max = -99999999
         maxTarget = 'democrat'
         for target in self.targets:
@@ -129,5 +81,4 @@ class Question2_Solver:
                 max = probDemRepub[target]
                 maxTarget = target
 
-        #print maxTarget
         return maxTarget
